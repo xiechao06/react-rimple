395cf66618ef0db1f0c6acf835f3a2a6078042c5
xiechao <xiechao06>
2019-01-21 15:22:25 +0800

    remove change callbacks in componentWillUnmount

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ export const withSlots = function withSlots (slots) {
       constructor (props) {
         super(props)
         let state = {}
+        this.changeCbs = {}
         for (let k in slots) {
           let slot = slots[k]
           // k is local to this closure
-          slot.change((value) => {
+          this.changeCbs[k] = (value) => {
             this.setState({ [k]: value })
-          })
+          }
+          slot.change(this.changeCbs[k])
           state[k] = slot.val()
         }
         this.state = state
       }
+
+      componentWillUnmount () {
+        for (let k in slots) {
+          slots[k].offChange(this.changeCbs[k])
+        }
+      }
+
       render () {
         let props = Object.assign({}, this.props, this.state, { slots })
         return <Component {...props} />
